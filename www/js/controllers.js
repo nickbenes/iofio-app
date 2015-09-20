94bba095528fdde4d85f80c2bc9485417a07a95d
Nick Benes <nickbenes@gmail.com>
2015-09-20 02:01:46 -0400

    Initial layout

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,40 @@
-angular.module('starter.controllers', [])
+angular.module('iofio.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+  .controller('MainCtrl', function ($ionicSideMenuDelegate) {
+    this.toggleLeft = function () {
+      $ionicSideMenuDelegate.toggleLeft();
+    }
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    this.nowPlaying = {
+      title: null,
+      src: null,
+      position: null,
+      duration: null,
+      state: null
+    };
+  })
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+  .controller('MenuCtrl', function () {
+    this.items = [
+      {
+        name: "Home",
+        ref: "page.home"
+      },
+      {
+        name: "Playlists",
+        ref: "page.playlist"
+      },
+      {
+        name: "Search",
+        ref: "page.search"
+      },
+      {
+        name: "Donations",
+        ref: "page.donations"
+      },
+      {
+        name: "Settings",
+        ref: "page.settings"
+      },
+    ]
+  });
